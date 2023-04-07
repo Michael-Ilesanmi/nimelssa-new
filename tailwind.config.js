59b06678a752918a29e191e7a1abfb3d382d89c6
Michael Ilesanmi <ioluwamichael@gmail.com>
2023-04-07 19:19:32 +0100

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      body: ['Roboto', 'sans-serif'],
+      heading: ['Raleway', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
